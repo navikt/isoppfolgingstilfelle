4558aed312e3d94ab3e8929ff3d35c62d443958a
Erik Jansen <erik.gunnar.jansen@nav.no>
2021-11-17 13:48:08 +0100

    Use const for pod metrics path

--- src/main/kotlin/no/nav/syfo/application/metric/api/MetricApi.kt
+++ src/main/kotlin/no/nav/syfo/application/metric/api/MetricApi.kt
@@ -5,8 +5,10 @@ import io.ktor.response.*
 import io.ktor.routing.*
 import no.nav.syfo.application.metric.METRICS_REGISTRY
 
+const val podMetricsPath = "/internal/metrics"
+
 fun Routing.registerMetricApi() {
-    get("/internal/metrics") {
+    get(podMetricsPath) {
         call.respondText(METRICS_REGISTRY.scrape())
     }
 }
