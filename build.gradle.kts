b3bf49c14ec45d3d10d52fab5646f0f8cab3f209
Erik Jansen <erik.gunnar.jansen@nav.no>
2021-12-16 10:24:29 +0100

    Bump dependencies to latest version

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,24 +5,24 @@ group = "no.nav.syfo"
 version = "1.0.0"
 
 object Versions {
-    const val flyway = "8.2.1"
+    const val flyway = "8.2.2"
     const val hikari = "5.0.0"
     const val jackson = "2.13.0"
     const val ktor = "1.6.7"
     const val kluent = "1.68"
-    const val logback = "1.2.7"
+    const val logback = "1.2.8"
     const val logstashEncoder = "7.0.1"
     const val mockk = "1.12.1"
     const val nimbusJoseJwt = "9.15.2"
-    const val micrometerRegistry = "1.8.0"
+    const val micrometerRegistry = "1.8.1"
     const val postgres = "42.3.1"
     const val postgresEmbedded = "0.13.4"
     const val spek = "2.0.17"
 }
 
 plugins {
-    kotlin("jvm") version "1.6.0"
-    id("com.github.johnrengelman.shadow") version "7.1.0"
+    kotlin("jvm") version "1.6.10"
+    id("com.github.johnrengelman.shadow") version "7.1.1"
     id("org.jlleitschuh.gradle.ktlint") version "10.2.0"
 }
 
@@ -66,7 +66,6 @@ dependencies {
     testImplementation("io.ktor:ktor-server-test-host:${Versions.ktor}")
     testImplementation("io.mockk:mockk:${Versions.mockk}")
     testImplementation("org.amshove.kluent:kluent:${Versions.kluent}")
-    testImplementation("org.spekframework.spek2:spek-dsl-jvm:${Versions.ktor}")
     testImplementation("org.spekframework.spek2:spek-dsl-jvm:${Versions.spek}") {
         exclude(group = "org.jetbrains.kotlin")
     }
