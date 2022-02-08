fea9bdabb451efeecf0d06c5915128e9015a2d17
Erik Jansen <erik.gunnar.jansen@nav.no>
2022-02-08 15:38:49 +0100

    Handle multiple SyketifelleBit for one PersonIdent in one poll correctly

--- src/main/kotlin/no/nav/syfo/oppfolgingstilfelle/OppfolgingstilfelleService.kt
+++ src/main/kotlin/no/nav/syfo/oppfolgingstilfelle/OppfolgingstilfelleService.kt
@@ -4,9 +4,14 @@ import no.nav.syfo.application.database.DatabaseInterface
 import no.nav.syfo.domain.PersonIdentNumber
 import no.nav.syfo.oppfolgingstilfelle.bit.*
 import no.nav.syfo.oppfolgingstilfelle.bit.database.createOppfolgingstilfelleBit
+import no.nav.syfo.oppfolgingstilfelle.bit.database.getOppfolgingstilfelleBitForUUID
+import no.nav.syfo.oppfolgingstilfelle.bit.kafka.COUNT_KAFKA_CONSUMER_SYKETILFELLEBIT_CREATED
+import no.nav.syfo.oppfolgingstilfelle.bit.kafka.COUNT_KAFKA_CONSUMER_SYKETILFELLEBIT_DUPLICATE
 import no.nav.syfo.oppfolgingstilfelle.database.*
 import no.nav.syfo.oppfolgingstilfelle.domain.OppfolgingstilfellePerson
 import no.nav.syfo.oppfolgingstilfelle.kafka.OppfolgingstilfelleProducer
+import no.nav.syfo.util.kafkaCallId
+import org.slf4j.LoggerFactory
 import java.sql.Connection
 
 class OppfolgingstilfelleService(
@@ -23,23 +28,66 @@ class OppfolgingstilfelleService(
         return oppfolgingstilfellePerson?.toOppfolgingstilfellePerson()
     }
 
-    fun createOppfolgingstilfellePerson(
+    fun createOppfolgingstilfelleBitList(
+        connection: Connection,
+        oppfolgingstilfelleBitList: List<OppfolgingstilfelleBit>
+    ) {
+        val oppfolgingstilfelleBitInPollCreatedList = mutableListOf<OppfolgingstilfelleBit>()
+        oppfolgingstilfelleBitList.forEach { oppfolgingstilfelleBit ->
+            log.info("Received relevant ${OppfolgingstilfelleBit::class.java.simpleName}, ready to process and attempt to create, inntruffet=${oppfolgingstilfelleBit.inntruffet}, callId=${kafkaCallId()}")
+
+            val isOppfolgingstilfelleBitDuplicate =
+                connection.getOppfolgingstilfelleBitForUUID(oppfolgingstilfelleBit.uuid) != null
+            if (isOppfolgingstilfelleBitDuplicate) {
+                log.warn(
+                    "No ${OppfolgingstilfelleBit::class.java.simpleName} was inserted into database, attempted to insert a duplicate"
+                )
+                COUNT_KAFKA_CONSUMER_SYKETILFELLEBIT_DUPLICATE.increment()
+            } else {
+                connection.createOppfolgingstilfelleBit(
+                    commit = false,
+                    oppfolgingstilfelleBit = oppfolgingstilfelleBit,
+                )
+                oppfolgingstilfelleBitInPollCreatedList.add(oppfolgingstilfelleBit)
+
+                val oppfolgingstilfelleBitInPollCreatedForPersonList =
+                    oppfolgingstilfelleBitInPollCreatedList.filter { createdBit ->
+                        createdBit.personIdentNumber.value == oppfolgingstilfelleBit.personIdentNumber.value
+                    }
+                createOppfolgingstilfellePerson(
+                    connection = connection,
+                    oppfolgingstilfelleBit = oppfolgingstilfelleBit,
+                    oppfolgingstilfelleBitInPollCreatedForPersonList = oppfolgingstilfelleBitInPollCreatedForPersonList,
+                )
+                COUNT_KAFKA_CONSUMER_SYKETILFELLEBIT_CREATED.increment()
+            }
+        }
+    }
+
+    private fun allCreatedOppfolgingstilfelleBit(
+        oppfolgingstilfelleBitInPollCreatedForPersonList: List<OppfolgingstilfelleBit>,
+    ): List<OppfolgingstilfelleBit> {
+        val oppfolgingstilfelleBitListCreatedBeforePoll = oppfolgingstilfelleBitService.oppfolgingstilfelleBitList(
+            personIdentNumber = oppfolgingstilfelleBitInPollCreatedForPersonList.first().personIdentNumber
+        )
+        val oppfolgingstilfelleBitForPersonList = oppfolgingstilfelleBitListCreatedBeforePoll
+            .toMutableList()
+        oppfolgingstilfelleBitForPersonList.addAll(oppfolgingstilfelleBitInPollCreatedForPersonList)
+        oppfolgingstilfelleBitForPersonList.sortedByDescending { bit -> bit.inntruffet }
+        return oppfolgingstilfelleBitForPersonList
+    }
+
+    private fun createOppfolgingstilfellePerson(
         connection: Connection,
         oppfolgingstilfelleBit: OppfolgingstilfelleBit,
+        oppfolgingstilfelleBitInPollCreatedForPersonList: List<OppfolgingstilfelleBit>,
     ) {
-        val oppfolgingstilfelleBitList = oppfolgingstilfelleBitService.oppfolgingstilfelleBitList(
-            personIdentNumber = oppfolgingstilfelleBit.personIdentNumber
-        ).toMutableList()
-        oppfolgingstilfelleBitList.add(oppfolgingstilfelleBit)
-        oppfolgingstilfelleBitList.sortedByDescending { bit -> bit.inntruffet }
-
-        val oppfolgingstilfelleList = oppfolgingstilfelleBitList.generateOppfolgingstilfelleList()
-        val oppfolgingstilfellePerson = oppfolgingstilfelleBit.toOppfolgingstilfellePerson(
-            oppfolgingstilfelleList = oppfolgingstilfelleList,
+        val oppfolgingstilfelleBitForPersonList = allCreatedOppfolgingstilfelleBit(
+            oppfolgingstilfelleBitInPollCreatedForPersonList = oppfolgingstilfelleBitInPollCreatedForPersonList
         )
-        connection.createOppfolgingstilfelleBit(
-            commit = false,
-            oppfolgingstilfelleBit = oppfolgingstilfelleBit,
+
+        val oppfolgingstilfellePerson = oppfolgingstilfelleBit.toOppfolgingstilfellePerson(
+            oppfolgingstilfelleBitList = oppfolgingstilfelleBitForPersonList,
         )
         connection.createOppfolgingstilfellePerson(
             commit = false,
@@ -49,5 +97,9 @@ class OppfolgingstilfelleService(
         oppfolgingstilfelleProducer.sendOppfolgingstilfelle(
             oppfolgingstilfellePerson = oppfolgingstilfellePerson
         )
+    }
+
+    companion object {
+        private val log = LoggerFactory.getLogger(OppfolgingstilfelleService::class.java)
     }
 }
--- src/main/kotlin/no/nav/syfo/oppfolgingstilfelle/bit/kafka/KafkaSyketilfellebitService.kt
+++ src/main/kotlin/no/nav/syfo/oppfolgingstilfelle/bit/kafka/KafkaSyketilfellebitService.kt
@@ -2,11 +2,8 @@ package no.nav.syfo.oppfolgingstilfelle.bit.kafka
 
 import no.nav.syfo.application.database.DatabaseInterface
 import no.nav.syfo.oppfolgingstilfelle.OppfolgingstilfelleService
-import no.nav.syfo.oppfolgingstilfelle.bit.database.getOppfolgingstilfelleBitForUUID
 import no.nav.syfo.oppfolgingstilfelle.bit.toOppfolgingstilfelleBit
-import no.nav.syfo.util.kafkaCallId
-import org.apache.kafka.clients.consumer.ConsumerRecords
-import org.apache.kafka.clients.consumer.KafkaConsumer
+import org.apache.kafka.clients.consumer.*
 import org.slf4j.LoggerFactory
 import java.sql.Connection
 import java.time.Duration
@@ -31,49 +28,57 @@ class KafkaSyketilfellebitService(
         consumerRecords: ConsumerRecords<String, KafkaSyketilfellebit>,
     ) {
         database.connection.use { connection ->
-            consumerRecords.forEach { consumerRecord ->
-                val callId = kafkaCallId()
-                if (consumerRecord.value() == null) {
-                    log.error("Value of ConsumerRecord is null, most probably due to a tombstone. Contact the owner of the topic if an error is suspected. key=${consumerRecord.key()} from topic: ${consumerRecord.topic()}, partiion=${consumerRecord.partition()}, offset=${consumerRecord.offset()}")
-                    COUNT_KAFKA_CONSUMER_SYKETILFELLEBIT_TOMBSTONE.increment()
-                    return
-                }
+            COUNT_KAFKA_CONSUMER_SYKETILFELLEBIT_READ.increment()
 
-                COUNT_KAFKA_CONSUMER_SYKETILFELLEBIT_READ.increment()
-                log.info("Received ${KafkaSyketilfellebit::class.java.simpleName}, ready to process. id=${consumerRecord.key()}, timestamp=${consumerRecord.timestamp()}, callId=$callId")
-
-                receiveKafkaSyketilfellebit(
-                    connection = connection,
-                    kafkaSyketilfellebit = consumerRecord.value(),
-                )
+            val (tombstoneRecordList, recordsValid) = consumerRecords.partition {
+                it.value() == null
             }
+            processTombstoneRecordList(
+                tombstoneRecordList = tombstoneRecordList,
+            )
+
+            val (relevantRecordList, notRelevantRecordList) = recordsValid.partition {
+                it.value().isRelevantForOppfolgingstilfelle()
+            }
+
+            processRelevantRecordList(
+                connection = connection,
+                relevantRecordList = relevantRecordList
+            )
+            processNotRelevantRecordList(
+                notRelevantRecordList = notRelevantRecordList,
+            )
             connection.commit()
         }
     }
 
-    private fun receiveKafkaSyketilfellebit(
-        connection: Connection,
-        kafkaSyketilfellebit: KafkaSyketilfellebit,
+    private fun processTombstoneRecordList(
+        tombstoneRecordList: List<ConsumerRecord<String, KafkaSyketilfellebit>>,
     ) {
-        if (kafkaSyketilfellebit.isRelevantForOppfolgingstilfelle()) {
-            val oppfolgingstilfelleBit = kafkaSyketilfellebit.toOppfolgingstilfelleBit()
+        if (tombstoneRecordList.isNotEmpty()) {
+            log.error("Value of ${tombstoneRecordList.size} ConsumerRecord are null, most probably due to a tombstone. Contact the owner of the topic if an error is suspected")
+            COUNT_KAFKA_CONSUMER_SYKETILFELLEBIT_TOMBSTONE.increment(tombstoneRecordList.size.toDouble())
+        }
+    }
 
-            val isOppfolgingstilfelleBitDuplicate =
-                connection.getOppfolgingstilfelleBitForUUID(oppfolgingstilfelleBit.uuid) != null
-            if (isOppfolgingstilfelleBitDuplicate) {
-                log.warn(
-                    "No ${KafkaSyketilfellebit::class.java.simpleName} was inserted into database, attempted to insert a duplicate"
-                )
-                COUNT_KAFKA_CONSUMER_SYKETILFELLEBIT_DUPLICATE.increment()
-            } else {
-                oppfolgingstilfelleService.createOppfolgingstilfellePerson(
-                    connection = connection,
-                    oppfolgingstilfelleBit = oppfolgingstilfelleBit,
-                )
-                COUNT_KAFKA_CONSUMER_SYKETILFELLEBIT_CREATED.increment()
-            }
-        } else {
-            COUNT_KAFKA_CONSUMER_SYKETILFELLEBIT_NOT_RELEVANT.increment()
+    private fun processRelevantRecordList(
+        connection: Connection,
+        relevantRecordList: List<ConsumerRecord<String, KafkaSyketilfellebit>>,
+    ) {
+        val relevantOppfolgingstilfelleBitList = relevantRecordList.map {
+            it.value().toOppfolgingstilfelleBit()
+        }
+        oppfolgingstilfelleService.createOppfolgingstilfelleBitList(
+            connection = connection,
+            oppfolgingstilfelleBitList = relevantOppfolgingstilfelleBitList,
+        )
+    }
+
+    private fun processNotRelevantRecordList(
+        notRelevantRecordList: List<ConsumerRecord<String, KafkaSyketilfellebit>>,
+    ) {
+        if (notRelevantRecordList.isNotEmpty()) {
+            COUNT_KAFKA_CONSUMER_SYKETILFELLEBIT_NOT_RELEVANT.increment(notRelevantRecordList.size.toDouble())
         }
     }
 
