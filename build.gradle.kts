f7c59152e641cd693e63b861e3156e9e9ecbc2ca
Geir Arne Waagbø <79845894+geir-waagboe@users.noreply.github.com>
2022-08-25 10:45:49 +0200

    Bump dependencies (#56)
    * Bump dependencies
    
    * Bump dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,8 +10,8 @@ object Versions {
     const val jackson = "2.13.3"
     const val jedis = "4.2.3"
     const val kafka = "3.2.0"
-    const val kafkaEmbedded = "3.1.4"
-    const val ktor = "2.0.3"
+    const val kafkaEmbedded = "3.2.1"
+    const val ktor = "2.1.0"
     const val kluent = "1.68"
     const val logback = "1.2.11"
     const val logstashEncoder = "7.2"
@@ -79,6 +79,14 @@ dependencies {
     }
     implementation("org.apache.kafka:kafka_2.13:${Versions.kafka}", excludeLog4j)
     testImplementation("no.nav:kafka-embedded-env:${Versions.kafkaEmbedded}", excludeLog4j)
+    constraints {
+        implementation("org.eclipse.jetty.http2:http2-server") {
+            because("no.nav:kafka-embedded-env:${Versions.kafkaEmbedded} -> https://advisory.checkmarx.net/advisory/vulnerability/CVE-2022-2048/")
+            version {
+                require("9.4.48.v20220622")
+            }
+        }
+    }
 
     testImplementation("com.nimbusds:nimbus-jose-jwt:${Versions.nimbusJoseJwt}")
     testImplementation("io.ktor:ktor-server-test-host:${Versions.ktor}")
