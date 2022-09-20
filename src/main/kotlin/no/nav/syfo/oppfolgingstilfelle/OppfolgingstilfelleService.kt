8d36178cb1ede3dcf60cd392d2e0b6711b4ed871
Luan Tran <8613937+luantr@users.noreply.github.com>
2022-09-20 13:16:06 +0200

    add oppfolgingstilfelleStartDateOrMinusFourMonthsDate function

--- src/main/kotlin/no/nav/syfo/oppfolgingstilfelle/OppfolgingstilfelleService.kt
+++ src/main/kotlin/no/nav/syfo/oppfolgingstilfelle/OppfolgingstilfelleService.kt
@@ -7,6 +7,9 @@ import no.nav.syfo.oppfolgingstilfelle.person.database.getOppfolgingstilfellePer
 import no.nav.syfo.oppfolgingstilfelle.person.database.toOppfolgingstilfellePerson
 import no.nav.syfo.oppfolgingstilfelle.person.domain.Oppfolgingstilfelle
 import no.nav.syfo.oppfolgingstilfelle.person.domain.OppfolgingstilfellePerson
+import no.nav.syfo.util.isAfterOrEqual
+import no.nav.syfo.util.isBeforeOrEqual
+import java.time.LocalDate
 
 class OppfolgingstilfelleService(
     val database: DatabaseInterface,
@@ -31,6 +34,26 @@ class OppfolgingstilfelleService(
             }
         }
         return allOppfolgingstilfelleList.sortedByDescending { tilfelle -> tilfelle.start }
+    }
+
+    /*
+    * Finds start date of oppfolgingstilfelle that overlaps with current date minus 4 months or else
+    * returns current date minus 4 months
+    * */
+    suspend fun oppfolgingstilfelleStartDateOrMinusFourMonthsDate(
+        callId: String,
+        personIdent: PersonIdentNumber,
+    ): LocalDate {
+        val oppfolgingstilfelleList = oppfolgingstilfelleList(callId, personIdent)
+
+        val minusFourMonthsDate = LocalDate.now().minusMonths(4)
+
+        val oppfolgingstilfelleStartDateOrMinusFourMonthsDate = oppfolgingstilfelleList
+            .find {
+                it.start.isBeforeOrEqual(minusFourMonthsDate) && it.end.isAfterOrEqual(minusFourMonthsDate)
+            }?.start ?: minusFourMonthsDate
+
+        return oppfolgingstilfelleStartDateOrMinusFourMonthsDate
     }
 
     private fun oppfolgingstilfellePerson(
