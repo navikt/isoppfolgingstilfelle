89b2e573a36ffc745e47f53fc961e5831a25cb0d
geir-waagboe <geir.arne.waagbo@nav.no>
2022-11-21 09:40:09 +0100

    Log info and count oppfolgingstilfelle from sykmelding-ny

--- src/main/kotlin/no/nav/syfo/oppfolgingstilfelle/person/metric/OppfolgingstilfelleMetric.kt
+++ src/main/kotlin/no/nav/syfo/oppfolgingstilfelle/person/metric/OppfolgingstilfelleMetric.kt
@@ -5,7 +5,6 @@ import no.nav.syfo.application.metric.METRICS_REGISTRY
 
 const val SYKMELDING_NY_COUNTER_NAME = "${METRICS_NS}_oppfolgingstilfelle_sykmelding_ny"
 
-
 val SYKMELDING_NY_COUNTER: Counter = Counter.builder(SYKMELDING_NY_COUNTER_NAME)
     .description("Counts the number of oppfolgingstilfeller > 118 days created only from sykmelding-ny")
     .register(METRICS_REGISTRY)
