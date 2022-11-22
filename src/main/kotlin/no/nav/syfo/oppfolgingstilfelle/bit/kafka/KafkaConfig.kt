c7c2f470c8b10b54bed8334198d973a9c3dd0852
geir-waagboe <geir.arne.waagbo@nav.no>
2022-11-22 14:37:13 +0100

    Reprocess tilfellebit

--- src/main/kotlin/no/nav/syfo/oppfolgingstilfelle/bit/kafka/KafkaConfig.kt
+++ src/main/kotlin/no/nav/syfo/oppfolgingstilfelle/bit/kafka/KafkaConfig.kt
@@ -12,13 +12,13 @@ fun kafkaSyketilfelleBitConsumerConfig(
     return Properties().apply {
         putAll(commonKafkaAivenConfig(kafkaEnvironment))
 
-        this[ConsumerConfig.GROUP_ID_CONFIG] = "isoppfolgingstilfelle-v3"
+        this[ConsumerConfig.GROUP_ID_CONFIG] = "isoppfolgingstilfelle-v4"
         this[ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG] = StringDeserializer::class.java.canonicalName
         this[ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG] =
             KafkaSyketilfellebitDeserializer::class.java.canonicalName
         this[ConsumerConfig.AUTO_OFFSET_RESET_CONFIG] = "earliest"
         this[ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG] = "false"
-        this[ConsumerConfig.MAX_POLL_RECORDS_CONFIG] = "10000"
+        this[ConsumerConfig.MAX_POLL_RECORDS_CONFIG] = "1000"
         this[ConsumerConfig.MAX_PARTITION_FETCH_BYTES_CONFIG] = "" + (10 * 1024 * 1024)
     }
 }
