3f2bc5fab752e2a562fe01e732987f6c8dc73839
Geir Arne Waagbø <79845894+geir-waagboe@users.noreply.github.com>
2023-03-28 16:35:49 +0200

    Allow tombstones (#130)

--- src/main/kotlin/no/nav/syfo/oppfolgingstilfelle/bit/kafka/KafkaSyketilfellebitDeserializer.kt
+++ src/main/kotlin/no/nav/syfo/oppfolgingstilfelle/bit/kafka/KafkaSyketilfellebitDeserializer.kt
@@ -3,10 +3,10 @@ package no.nav.syfo.oppfolgingstilfelle.bit.kafka
 import no.nav.syfo.util.configuredJacksonMapper
 import org.apache.kafka.common.serialization.Deserializer
 
-class KafkaSyketilfellebitDeserializer : Deserializer<KafkaSyketilfellebit> {
+class KafkaSyketilfellebitDeserializer : Deserializer<KafkaSyketilfellebit?> {
     private val mapper = configuredJacksonMapper()
 
-    override fun deserialize(topic: String, data: ByteArray): KafkaSyketilfellebit =
+    override fun deserialize(topic: String, data: ByteArray): KafkaSyketilfellebit? =
         mapper.readValue(data, KafkaSyketilfellebit::class.java)
 
     override fun close() {}
