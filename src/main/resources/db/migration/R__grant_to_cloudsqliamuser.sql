03a2655d9fd2b0941b923f05db10dd11a12b61af
geir-waagboe <geir.arne.waagbo@nav.no>
2023-04-11 15:27:26 +0200

    Tombstone records should be deleted

--- src/main/resources/db/migration/R__grant_to_cloudsqliamuser.sql
+++ src/main/resources/db/migration/R__grant_to_cloudsqliamuser.sql
@@ -1,3 +1,4 @@
 REVOKE ALL ON ALL TABLES IN SCHEMA public FROM cloudsqliamuser;
+-- grant select
 GRANT SELECT ON ALL TABLES IN SCHEMA public TO cloudsqliamuser;
 GRANT SELECT ON ALL TABLES IN SCHEMA public TO "isyfo-analyse";
