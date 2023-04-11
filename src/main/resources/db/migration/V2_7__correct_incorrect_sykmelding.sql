1a3994f799f69bac60b7183adfdcab84a6309195
geir-waagboe <geir.arne.waagbo@nav.no>
2023-04-11 10:06:10 +0200

    Fix incorrect tom for old tilfellebiter

---
+++ src/main/resources/db/migration/V2_7__correct_incorrect_sykmelding.sql
@@ -0,0 +1,4 @@
+UPDATE TILFELLE_BIT SET tom='2020-08-14' where uuid='29793c9f-be76-32e5-9cfb-44b44ba06672';
+UPDATE TILFELLE_BIT SET tom='2020-08-14' where uuid='7de9b656-f36a-3b0e-bcb0-1ecc09675e97';
+UPDATE TILFELLE_BIT SET processed=false where uuid='d3bfbc56-7d10-4e7a-a803-8856c4ffffeb';
+UPDATE TILFELLE_BIT SET processed=false where uuid='a0ed3ba8-275e-402f-8eb6-5a33ff0eb420';
