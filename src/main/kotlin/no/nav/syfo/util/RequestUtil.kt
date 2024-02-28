47423a1025479f4c233098b151e2e2d274e1c482
Anders Rognstad <79838644+andersrognstad@users.noreply.github.com>
2024-02-28 13:36:50 +0100

    IS-2112: Add behandlingsnummer header to pdl request (#174)

--- src/main/kotlin/no/nav/syfo/util/RequestUtil.kt
+++ src/main/kotlin/no/nav/syfo/util/RequestUtil.kt
@@ -1,23 +1,9 @@
 package no.nav.syfo.util
 
-import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter
-import java.util.concurrent.atomic.AtomicInteger
-
 const val NAV_CALL_ID_HEADER = "Nav-Call-Id"
 
 const val NAV_PERSONIDENT_HEADER = "nav-personident"
 
 const val NAV_VIRKSOMHETSNUMMER = "nav-virksomhetsnummer"
 
-const val ALLE_TEMA_HEADERVERDI = "GEN"
-const val TEMA_HEADER = "Tema"
-
 fun bearerHeader(token: String) = "Bearer $token"
-
-private val kafkaCounter = AtomicInteger(0)
-
-fun kafkaCallId(): String =
-    "${
-    LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd-MM-HHmm"))
-    }-isoppfolgingstilfelle-kafka-${kafkaCounter.incrementAndGet()}"
