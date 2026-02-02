9a047001c6759391875fdb1dd84e84d1566fdb31
Daniel Engebretsen <daniel.engebretsen@nav.no>
2026-02-02 12:36:40 +0100

    IS-3669: remove test user data

---
+++ src/main/resources/db/migration/V3_8__delete_test_user_data.sql
@@ -0,0 +1,7 @@
+DELETE FROM oppfolgingstilfelle_person
+WHERE id = 93272317
+  AND uuid = 'b275eb14-ec39-4d62-9744-d43384f6a241';
+
+DELETE FROM tilfelle_bit
+WHERE id = 92798155
+  AND uuid = '1fedeefa-00b0-4f61-82da-ee204fc08fda';
